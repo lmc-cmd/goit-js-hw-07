485c7f833aab2104dd39f8914808601df88e2fa7
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-29 17:26:51 +0300

    hw 7.1 & 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeGallery = galleryItems.map((e) => {
+  // const galarryItem = document.createElement(`div`);
+  // galarryItem.classList.add(`gallery__item`);
+  const makeItemLink = () => {
+    const galleryItemLink = document.createElement(`a`);
+    galleryItemLink.classList.add(`gallery__link`);
+    galleryItemLink.href = e.original;
+    const makeImg = () => {
+      const imgItem = document.createElement(`img`);
+      imgItem.classList.add(`gallery__image`);
+      imgItem.src = e.preview;
+      // imgItem.dataSource = e.original;
+      imgItem.alt = e.description;
+      return imgItem;
+    };
+    galleryItemLink.append(makeImg());
+    return galleryItemLink;
+  };
+  galleryEl.append(makeItemLink());
+  // return galarryItem;
+});
+// galleryEl.append(...makeGallery);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionPosition: "bottom",
+  captionData: "alt",
+  navtext: ["<~", "~>"],
+});
