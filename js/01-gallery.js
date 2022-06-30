413a70736ae1012d01e1d360ce0d8369f3c6fb7f
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-30 16:38:42 +0300

    try2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,73 +1,44 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryEl = document.querySelector(".gallery");
 
-const makeGallery = galleryItems.map((e) => {
+const makeGalleryArr = galleryItems.map((e) => {
   const galarryItemEl = `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src=${e.preview}
-      data-source=${e.original}
-      alt=${e.description}
-    />
+  <a class="gallery__link" href="${e.original}">
+  <img
+  class="gallery__image"
+  src=${e.preview}
+  data-source=${e.original}
+  alt=${e.description}
+  />
   </a>
   </div>`;
-  console.log(galarryItemEl);
   return galarryItemEl;
-  //   const galarryItem = document.createElement(`div`);
-  //   galarryItem.classList.add(`gallery__item`);
-  //   const makeItemLink = () => {
-  //     const galleryItemLink = document.createElement(`a`);
-  //     galleryItemLink.classList.add(`gallery__link`);
-  //     const makeImg = () => {
-  //       const imgItem = document.createElement(`img`);
-  //       imgItem.classList.add(`gallery__image`);
-  //       imgItem.src = e.preview;
-  //       imgItem.dataSource = e.original;
-  //       imgItem.alt = e.description;
-  //       return imgItem;
-  //     };
-  //     galleryItemLink.append(makeImg());
-  //     return galleryItemLink;
-  //   };
-  //   galarryItem.append(makeItemLink());
-  //   return galarryItem;
-
-  // const galaryEl = document.querySelector(`.gallery`);
-  // const makeGaleryEL = ({ url, alt } = {}) =>
-  //   `<li class="gallery__item"><a href=""><img src=${url} alt=${alt} width="300" height="200"
-  //     style= margin:20px></a></li>`;
-
-  // const makeGaleryBrutforce = images.map((el) => makeGaleryEL(el)).join("");
-  // galaryEl.insertAdjacentHTML("beforeend", makeGaleryBrutforce);
 });
-makeGallery.join("");
-galleryEl.append(...makeGallery);
-// galarryItemEl.insertAdjacentHTML("beforeend");
+
+const makeGallery = makeGalleryArr.join("");
+galleryEl.insertAdjacentHTML("afterbegin", makeGallery);
 
 galleryEl.addEventListener(`click`, (e) => {
-  if (e.target.nodeName === `DIV`) {
-    return;
-  }
-  const instance = basicLightbox.create(
-    `<div class="modal">
-    <img
-    src=${e.target.dataSource}
-    />
-    </div>`
-  );
-  instance.show(instance);
+  e.preventDefault();
+  if (e.target.nodeName === `IMG`) {
+    const instance = basicLightbox.create(
+      `<div class="modal">
+      <img
+      src=${e.target.dataset.source}
+      />
+      </div>`
+    );
+    instance.show(instance);
 
-  const modalEl = document.querySelector(".basicLightbox");
+    const modalEl = document.querySelector(".basicLightbox");
 
-  modalEl.addEventListener(`click`, (e) => {
-    if (e.target.nodeName === `IMG`) {
-      instance.close(instance);
+    modalEl.addEventListener(`click`, (e) => {
+      if (e.target) {
+        instance.close(instance);
+      }
       modalEl.removeEventListener;
-    }
-  });
+    });
+  }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,32 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 
 const galleryEl = document.querySelector(".gallery");
 
-const makeGallery = galleryItems.map((e) => {
-  // const galarryItem = document.createElement(`div`);
-  // galarryItem.classList.add(`gallery__item`);
-  const makeItemLink = () => {
-    const galleryItemLink = document.createElement(`a`);
-    galleryItemLink.classList.add(`gallery__link`);
-    galleryItemLink.href = e.original;
-    const makeImg = () => {
-      const imgItem = document.createElement(`img`);
-      imgItem.classList.add(`gallery__image`);
-      imgItem.src = e.preview;
-      // imgItem.dataSource = e.original;
-      imgItem.alt = e.description;
-      return imgItem;
-    };
-    galleryItemLink.append(makeImg());
-    return galleryItemLink;
-  };
-  galleryEl.append(makeItemLink());
-  // return galarryItem;
+const makeGalleryArr = galleryItems.map((e) => {
+  const galarryItemEl = `
+  <a class="gallery__item" href=${e.original}>
+  <img class="gallery__image" src=${e.preview} alt=${e.description} />
+</a>`;
+  return galarryItemEl;
 });
-// galleryEl.append(...makeGallery);
+
+const makeGallery = makeGalleryArr.join("");
+galleryEl.insertAdjacentHTML("afterbegin", makeGallery);
 
 new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionPosition: "bottom",
-  captionData: "alt",
+  captionsData: "alt",
   navtext: ["<~", "~>"],
 });
+console.dir(SimpleLightbox);
