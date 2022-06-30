ff34666aa48312606022d778dfb76c60ea4749a6
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-06-30 10:00:07 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,26 +6,48 @@ import { galleryItems } from "./gallery-items.js";
 const galleryEl = document.querySelector(".gallery");
 
 const makeGallery = galleryItems.map((e) => {
-  const galarryItem = document.createElement(`div`);
-  galarryItem.classList.add(`gallery__item`);
-  const makeItemLink = () => {
-    const galleryItemLink = document.createElement(`a`);
-    galleryItemLink.classList.add(`gallery__link`);
-    const makeImg = () => {
-      const imgItem = document.createElement(`img`);
-      imgItem.classList.add(`gallery__image`);
-      imgItem.src = e.preview;
-      imgItem.dataSource = e.original;
-      imgItem.alt = e.description;
-      return imgItem;
-    };
-    galleryItemLink.append(makeImg());
-    return galleryItemLink;
-  };
-  galarryItem.append(makeItemLink());
-  return galarryItem;
+  const galarryItemEl = `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${e.preview}
+      data-source=${e.original}
+      alt=${e.description}
+    />
+  </a>
+  </div>`;
+  console.log(galarryItemEl);
+  return galarryItemEl;
+  //   const galarryItem = document.createElement(`div`);
+  //   galarryItem.classList.add(`gallery__item`);
+  //   const makeItemLink = () => {
+  //     const galleryItemLink = document.createElement(`a`);
+  //     galleryItemLink.classList.add(`gallery__link`);
+  //     const makeImg = () => {
+  //       const imgItem = document.createElement(`img`);
+  //       imgItem.classList.add(`gallery__image`);
+  //       imgItem.src = e.preview;
+  //       imgItem.dataSource = e.original;
+  //       imgItem.alt = e.description;
+  //       return imgItem;
+  //     };
+  //     galleryItemLink.append(makeImg());
+  //     return galleryItemLink;
+  //   };
+  //   galarryItem.append(makeItemLink());
+  //   return galarryItem;
+
+  // const galaryEl = document.querySelector(`.gallery`);
+  // const makeGaleryEL = ({ url, alt } = {}) =>
+  //   `<li class="gallery__item"><a href=""><img src=${url} alt=${alt} width="300" height="200"
+  //     style= margin:20px></a></li>`;
+
+  // const makeGaleryBrutforce = images.map((el) => makeGaleryEL(el)).join("");
+  // galaryEl.insertAdjacentHTML("beforeend", makeGaleryBrutforce);
 });
+makeGallery.join("");
 galleryEl.append(...makeGallery);
+// galarryItemEl.insertAdjacentHTML("beforeend");
 
 galleryEl.addEventListener(`click`, (e) => {
   if (e.target.nodeName === `DIV`) {
